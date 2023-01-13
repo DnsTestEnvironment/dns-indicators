5cc8c5323f71135365ac3a1e5552d8d1ea3e9611
Moritz Bosse <moritz.bosse@destatis.de>
2023-01-13 17:09:11 +0100

    2.8

--- _includes/assets/js/model/chartHelpers.js
+++ _includes/assets/js/model/chartHelpers.js
@@ -426,7 +426,7 @@ function getCombinationType(combination, fallback, mixedTypes) {
     return 'line';
   }
   else {
-    console.log("MT",mixedTypes);
+    console.log("MT", typeof mixedTypes);
     return 'bar';//mixedTypes.find(item => item.combination === combi).chartType;
   }
 }
