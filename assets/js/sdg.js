b3aac081ba444c4376d18197f5f3e7de79d7ee2d
MoBosse <50657629+MoBosse@users.noreply.github.com>
2023-05-08 08:20:12 +0000

    Deploying to gh-pages from  @ e827fadea2ac4b375155d7cae8a15860716c34fe 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -6494,3 +6494,11 @@ $(function() {
 $(document).ready(function() {
     $('a[href="#top"]').prepend('<svg class="app-c-back-to-top__icon" xmlns="http://www.w3.org/2000/svg" width="13" height="17" viewBox="0 0 13 17" aria-hidden="true" focusable="false"><path fill="currentColor" d="M6.5 0L0 6.5 1.4 8l4-4v12.7h2V4l4.3 4L13 6.4z"></path></svg>');
 });
+function confirm_alert(source) {
+  if (source && source != {
+    var text = 'Sie verlassen unsere Webseite!\nDer Link führt Sie zur Webseite '
+    return confirm(text + source + '.');
+  } else {
+    return confirm('Sie verlassen unsere Webseite!\nDer Link führt Sie zu einer externen Webseite.')
+  }
+}
