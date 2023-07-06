e30e23e58ce4c9d8d0394cea8f7c42af0ecd7490
Moritz Bosse <moritz.bosse@destatis.de>
2023-07-06 08:34:59 +0200

    2.3

--- _includes/assets/js/indicatorCleanupForIframe.js
+++ _includes/assets/js/indicatorCleanupForIframe.js
@@ -1,5 +1,6 @@
 function deleteStuffFromIndicatorPagesForIframe(indicator) {
     var iframeId = 'myIframe_' + indicator;
+    var goal = str(indicatort.substring(0, indicatort.search('-')));
     var iframe = document.getElementById(iframeId);
     var iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
     // Header ausblenden
@@ -8,7 +9,7 @@ function deleteStuffFromIndicatorPagesForIframe(indicator) {
         header.style.display = "none";
     }
     // Element mit Klasse "heading goal-banner indicator goal-2" ausblenden
-    var element = iframeDocument.querySelector(".heading.goal-banner.indicator.goal-2");
+    var element = iframeDocument.querySelector(".heading.goal-banner.indicator.goal-" + goal);
     if (element) {
         element.style.display = "none";
     }
