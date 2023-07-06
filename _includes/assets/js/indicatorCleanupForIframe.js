6cd6877085c6fdbed1147eb9e9c10bd3ae585eaf
Moritz Bosse <moritz.bosse@destatis.de>
2023-07-06 08:57:05 +0200

    2.4

--- _includes/assets/js/indicatorCleanupForIframe.js
+++ _includes/assets/js/indicatorCleanupForIframe.js
@@ -26,4 +26,32 @@ function deleteStuffFromIndicatorPagesForIframe(indicator) {
     iframeDocument.querySelectorAll(".app-c-back-to-top__icon").forEach(a=>a.style.display = "none");
     // Footer ausblenden
     iframeDocument.querySelectorAll('[role="contentinfo"]').forEach(a=>a.style.display = "none");
+  };
+
+function deleteStuffFromIndicatorPagesForIframe2(iframeDocument, indicator) {
+    var iframeId = 'myIframe_' + indicator;
+    var goal = str(indicator.substring(0, indicator.search('-')));
+    // Header ausblenden
+    var header = iframeDocument.querySelector("header[role='banner']");
+    if (header) {
+        header.style.display = "none";
+    }
+    // Element mit Klasse "heading goal-banner indicator goal-2" ausblenden
+    var element = iframeDocument.querySelector(".heading.goal-banner.indicator.goal-" + goal);
+    if (element) {
+        element.style.display = "none";
+    }
+    // Disclaimer ausblenden
+    var disclaimer = iframeDocument.getElementById("disclaimer");
+    if (disclaimer) {
+        disclaimer.style.display = "none";
+    }
+    // Breadcrumbs ausblenden
+    iframeDocument.querySelectorAll(".breadcrumb").forEach(a=>a.style.display = "none");
+    // Buttons ausblenden
+    iframeDocument.getElementById("navigationbuttons").style.display = 'none';
+    // Buttons ausblenden
+    iframeDocument.querySelectorAll(".app-c-back-to-top__icon").forEach(a=>a.style.display = "none");
+    // Footer ausblenden
+    iframeDocument.querySelectorAll('[role="contentinfo"]').forEach(a=>a.style.display = "none");
   }
