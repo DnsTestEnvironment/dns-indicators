2c5d7a1b91aeba73644f44a73eee6c74039fe8fc
MoBosse <50657629+MoBosse@users.noreply.github.com>
2025-02-07 11:03:52 +0000

    Deploying to gh-pages from  @ 460e0af870aab01e285d252e4e8303b6654e54d2 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -3806,6 +3806,7 @@ function getChartConfig(chartInfo) {
         console.log('Unknown chart type: ' + chartType + '. Falling back to "line".');
         chartType = 'line';
     }
+    console.log('chartInfo: ' + chartInfo);
     return opensdg.chartTypes[chartType](chartInfo);
 }
 
