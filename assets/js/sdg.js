31f46ef673f69098b4cc1c91720ee31081596246
MoBosse <50657629+MoBosse@users.noreply.github.com>
2025-02-12 15:12:01 +0000

    Deploying to gh-pages from  @ c7b71b419adc95592b1871e2bc55bb230cc7365c 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -2607,8 +2607,8 @@ function getCombinationType(combination, fallback, mixedTypes) {
       return '';
     }
     else {
-      console.log("AB", typeof mixedTypes, mixedTypes, combi, combination, getCombinationDescription([item.field, item.value],''));
       return mixedTypes.find(function(item) {
+        console.log("AB", typeof mixedTypes, mixedTypes, combi, combination, getCombinationDescription([item.field, item.value],''));
         return getCombinationDescription([item.field, item.value],'') === combination;
       });
       //return '';//mixedTypes.find(item => item.combination === combi).chartType;
