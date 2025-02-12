959b787fa98b5bbd4e5db70ddf18c1dab399b3aa
MoBosse <50657629+MoBosse@users.noreply.github.com>
2025-02-12 10:52:23 +0000

    Deploying to gh-pages from  @ 252663ab5036b184b33ee524adeee06b3a49f585 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -2701,6 +2701,7 @@ function makeHeadlineDataset(years, rows, label, showLine, spanGaps, colors, all
     observationAttributes: obsAttributes,
     showLine: showLine,
     spanGaps: spanGaps,
+    mixedTypes: mixedTypes,
     type: getCombinationType([], '', mixedTypes),
   });
 }
