8790b10bf32430b294b2ffd72bcbf05e916100f1
MoBosse <50657629+MoBosse@users.noreply.github.com>
2025-02-12 15:21:10 +0000

    Deploying to gh-pages from  @ a52512ed26f329625155d1eb17a4838a136855a8 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -2609,7 +2609,8 @@ function getCombinationType(combination, fallback, mixedTypes) {
     else {
       return mixedTypes.find(function(item) {
         console.log("AB", typeof mixedTypes, mixedTypes, combi, combination, getCombinationDescription([item.field, item.value],''));
-        return getCombinationDescription([item.field, item.value],'') === combination;
+        console.log("ABx", getCombinationDescription([item.value],'') === combination);
+        return getCombinationDescription([item.value],'') === combination;
       });
       //return '';//mixedTypes.find(item => item.combination === combi).chartType;
     }
