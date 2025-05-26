f423681c83e02714dfade47d01c3a3654dcb133f
MoBosse <50657629+MoBosse@users.noreply.github.com>
2025-05-26 07:12:24 +0000

    Deploying to gh-pages from  @ 7b8d3cbed074c92f740bb68545329b80762b5ac5 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -4544,8 +4544,15 @@ opensdg.chartTypes.base = function(info) {
     }
 
     // Manually set the borderWidths to 0 to avoid a weird border effect on the bars.
+    // exception for line datasets in a mixed chart
     config.data.datasets.forEach(function(dataset) {
+      if (dataset.type == 'line') {
+        dataset.borderWidth = 2;
+      }
+      else {
         dataset.borderWidth = 0;
+      }
+        //dataset.borderWidth = 0;
     });
     // Add these overrides onto the normal config, and return it.
     _.merge(config, overrides);
