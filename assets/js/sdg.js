1228a66ffaf97e72226272252511e844acf298a8
MoBosse <50657629+MoBosse@users.noreply.github.com>
2025-07-04 09:45:34 +0000

    Deploying to gh-pages from  @ a562037ed4c2960ac440867116c0e0cc49423328 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -4236,7 +4236,7 @@ opensdg.chartTypes.base = function(info) {
                             }
                             return line;
                           } else {
-                            return label + ': ' + alterDataDisplay(tooltipItem.raw, tooltipItem.dataset, 'chart tooltip', tooltipItem);
+                            return label + ': ' + alterDataDisplay(tooltipItem.raw, tooltipItem.dataset, 'chart tooltip', tooltipItem).replace("<u>","").replace("</u>","");
                           }
                         },
                         afterLabel: function(tooltipItem) {
