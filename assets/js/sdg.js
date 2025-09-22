913102cf9e2d28cff9cd6def9098637c9660fc1b
MoBosse <50657629+MoBosse@users.noreply.github.com>
2025-09-22 13:53:50 +0000

    Deploying to gh-pages from  @ 0de7f84fda4e86ae7f1d647ab08195714d46cf47 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -5461,7 +5461,7 @@ function createIndicatorDownloadButtons(indicatorDownloads, indicatorId, el) {
     MODEL.onFieldsComplete.attach(function (sender, args) {
 
         helpers.initialiseFields(args);
-
+        console.log("ARGS",args);
         if (args.hasGeoData && args.showMap) {
             VIEW._mapView = new mapView();
             VIEW._mapView.initialise(
