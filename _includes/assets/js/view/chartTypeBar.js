9e3e54dcd70fdb658d969a881aa5412801339b47
Moritz Bosse <moritz.bosse@destatis.de>
2025-09-26 12:22:57 +0200

    1.2

--- _includes/assets/js/view/chartTypeBar.js
+++ _includes/assets/js/view/chartTypeBar.js
@@ -2,6 +2,7 @@ opensdg.chartTypes.bar = function (info) {
     var config = opensdg.chartTypes.base(info);
     var overrides = {
         type: 'bar',
+        borderWidth: 0;
     };
     if (info.stackedDisaggregation) {
         console.log('Stacked', info.stackedDisaggregation, typeof info.stackedDisaggregation);
