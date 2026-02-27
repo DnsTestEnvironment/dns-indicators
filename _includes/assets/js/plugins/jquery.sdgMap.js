b5f7e201bb73ad4547e851d3d74f71eb2613beba
Moritz Bosse <moritz.bosse@destatis.de>
2026-02-27 16:05:02 +0100

    redo

--- _includes/assets/js/plugins/jquery.sdgMap.js
+++ _includes/assets/js/plugins/jquery.sdgMap.js
@@ -57,7 +57,7 @@
   function Plugin(element, options) {
 
     this.element = element;
-
+    console.log("ELEMENT",element);
     // Support colorRange map option in string format.
     if (typeof options.mapOptions.colorRange === 'string') {
       var colorRangeParts = options.mapOptions.colorRange.split('.'),
@@ -101,6 +101,7 @@
     this.startValues = options.startValues;
     this.configObsAttributes = {{ site.observation_attributes | jsonify }};
     this.allObservationAttributes = options.allObservationAttributes;
+    this._browserDecimalSeparator = this.viewHelpers.getBrowserDecimalSeparator();
 
     // Require at least one geoLayer.
     if (!options.mapLayers || !options.mapLayers.length) {
@@ -307,23 +308,24 @@
       opensdg.dataDisplayAlterations.forEach(function(callback) {
         value = callback(value);
       });
-      if (this._precision || this._precision === 0) {
-        value = Number.parseFloat(value).toFixed(this._precision);
-      }
-      if (this._decimalSeparator) {
-        if(opensdg.language == 'de') {
+      if (typeof value !== 'number') {
+        if (this._precision || this._precision === 0) {
+          value = Number.parseFloat(value).toFixed(this._precision);
+        }
+        if (this._decimalSeparator) {
           value = value.toString().replace('.', this._decimalSeparator);
         }
-        else {
-          value = value.toString();
-        }
       }
-      if (this._thousandsSeparator) {
-        if(opensdg.language == 'de') {
-          value = value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, this._thousandsSeparator);
+      else {
+        var localeOpts = {};
+        if (this._precision || this._precision === 0) {
+            localeOpts.minimumFractionDigits = this._precision;
+            localeOpts.maximumFractionDigits = this._precision;
         }
-        else {
-          value = value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+        value = value.toLocaleString(opensdg.language, localeOpts);
+        // Still use the custom decimal separator if it is there.
+        if (this._decimalSeparator) {
+          value = value.toString().replace(this._browserDecimalSeparator, this._decimalSeparator);
         }
       }
       return value;
@@ -331,6 +333,7 @@
 
     // Get the data from a feature's properties, according to the current year.
     getData: function(props) {
+      console.log("Props",props);
       var ret = false;
       if (props.values && props.values.length && this.currentDisaggregation < props.values.length) {
         var value = props.values[this.currentDisaggregation][this.currentYear];
