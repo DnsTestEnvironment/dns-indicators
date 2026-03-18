c8763fc3ff0ca7c4afb25f6fb9dee5cbdcec7e47
Moritz Bosse <moritz.bosse@destatis.de>
2026-03-18 10:47:29 +0100

    test fill

--- _includes/assets/js/model/chartHelpers.js
+++ _includes/assets/js/model/chartHelpers.js
@@ -382,7 +382,7 @@ function makeDataset(years, rows, combination, labelFallback, color, background,
        prepared = prepareDataForDataset(years, rows, allObservationAttributes),
        data = prepared.data,
        obsAttributes = prepared.observationAttributes;
-  console.log("getCOmType:", getCombinationType(combination, labelFallback, mixedTypes) );
+
   return Object.assign(dataset, {
     label: getCombinationDescription(combination, labelFallback),
     combination: combination,
--- _includes/assets/js/view/chartTypeBase.js
+++ _includes/assets/js/view/chartTypeBase.js
@@ -22,6 +22,7 @@ opensdg.chartTypes.base = function(info) {
             responsive: true,
             maintainAspectRatio: false,
             spanGaps: true,
+            fill: 'origin',
             scrollX: true,
             scrollCollapse: true,
             sScrollXInner: '150%',
