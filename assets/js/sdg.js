14933d9bafc738a21569edf3fcd9c718ab163ea7
MoBosse <50657629+MoBosse@users.noreply.github.com>
2026-03-20 11:23:47 +0000

    Deploying to gh-pages from  @ 0cab778d01a52b9cda0c01bcd32e425d78fc12a5 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -2701,11 +2701,11 @@ function getCombinationDescription(combination, fallback) {
  */
 function getFilling(fill) {
   if (fill) {
-    return {
+    return [{
       target: 'origin',
       above: 'rgb(255, 0, 0)',   // Area will be red above the origin
       below: 'rgb(0, 0, 255)'    // And blue below the origin
-    }
+    }]
   }
   else {
     return false
