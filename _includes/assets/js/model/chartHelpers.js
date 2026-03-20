d4c46209981b75468f272a58482c22de94aaa733
Moritz Bosse <moritz.bosse@destatis.de>
2026-03-20 13:40:24 +0100

    5.1

--- _includes/assets/js/model/chartHelpers.js
+++ _includes/assets/js/model/chartHelpers.js
@@ -399,7 +399,11 @@ function makeDataset(years, rows, combination, labelFallback, color, background,
     pointStyle: 'circle',
     data: data,
     excess: excess,
-    fill: 'origin',
+    fill: {
+      target: fill.target,
+      above: fill.above,
+      below: fill.below
+    },
     spanGaps: spanGaps,
     showLine: showLine,
     observationAttributes: obsAttributes,
@@ -551,7 +555,11 @@ function makeHeadlineDataset(years, rows, label, fill, showLine, spanGaps, color
     pointStyle: 'circle',
     data: data,
     observationAttributes: obsAttributes,
-    fill: 'origin',
+    fill: {
+      target: fill.target,
+      above: fill.above,
+      below: fill.below
+    },
     showLine: showLine,
     spanGaps: spanGaps,
     type: getCombinationType([], '', mixedTypes),
