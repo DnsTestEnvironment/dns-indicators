c07d29d804c354094c4cffa4a0e935ec8fdeb85f
Moritz Bosse <moritz.bosse@destatis.de>
2026-05-18 15:32:10 +0200

    4.0

--- _includes/assets/js/model/helpers.js
+++ _includes/assets/js/model/helpers.js
@@ -60,6 +60,7 @@
     YEAR_COLUMN: YEAR_COLUMN,
     VALUE_COLUMN: VALUE_COLUMN,
     GRAPH_TITLE_FROM_SERIES: GRAPH_TITLE_FROM_SERIES,
+    COMMENT_OBS_0: COMMENT_OBS_0,
     convertJsonFormatToRows: convertJsonFormatToRows,
     getUniqueValuesByProperty: getUniqueValuesByProperty,
     dataHasUnits: dataHasUnits,
