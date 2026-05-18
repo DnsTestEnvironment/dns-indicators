b399abc1b01f9fd87dbb58bfc8aeafbef345c315
Moritz Bosse <moritz.bosse@destatis.de>
2026-05-18 11:37:24 +0200

    1.4

--- _includes/assets/js/plugins/jquery.sdgMap.js
+++ _includes/assets/js/plugins/jquery.sdgMap.js
@@ -308,8 +308,7 @@
       opensdg.dataDisplayAlterations.forEach(function(callback) {
         value = callback(value);
       });
-      //get rid of observationAttributes
-      value = value.replace(value.match(/\[(.*?)\]/)[0],'')/1;
+      
       if (typeof value !== 'number') {
         if (this._precision || this._precision === 0) {
           value = Number.parseFloat(value).toFixed(this._precision);
