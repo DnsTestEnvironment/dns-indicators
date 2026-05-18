f1d36ee065ca7a3868d7f37782533304dbe4cfc4
MoBosse <50657629+MoBosse@users.noreply.github.com>
2026-05-18 05:08:14 +0000

    Deploying to gh-pages from  @ 07838f9d2a9cabd9286220e4315100c7c73b60a4 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -5234,6 +5234,17 @@ function getObservationAttributeFootnoteSymbol(obsAttribute) {
     //return '[' + translations.indicator.note + ' ' + (num + 1) + ']';
 }
 
+/**
+ * Figure out what the browser will be using for the decimal separator.
+ *
+ * @returns {string} The decimal separator the browser will use.
+ */
+function getBrowserDecimalSeparator() {
+    var browserDecimal = 1.1;
+    browserDecimal = browserDecimal.toLocaleString(opensdg.language).substring(1, 2);
+    return browserDecimal;
+}
+
   /**
  * @param {String} selectedSeries
  * @param {String} selectedUnit
