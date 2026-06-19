c96c9a7ec8d771ae62a01911610cd7deb9e45475
Moritz Bosse <moritz.bosse@destatis.de>
2026-06-19 08:05:17 +0200

    1.4

--- _includes/assets/js/plugins/jquery.sdgMap.js
+++ _includes/assets/js/plugins/jquery.sdgMap.js
@@ -206,7 +206,7 @@
               //footnoteNumbers.push(plugin.viewHelpers.getObservationAttributeFootnoteSymbol(footnoteNumber));
             }
           });
-          if (footnoteNumbers.length > 0) {
+          if (tooltipAtts.length > 0) {
             console.log("tooltipAtts:", tooltipAtts);
             tooltipContent += ' [' + tooltipAtts.join(', ') + ']';
           }
